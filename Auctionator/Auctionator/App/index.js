b61ec7a12809fd15f3511e43218babebaa05d9e8
igorjouk <igrokk70@gmail.com>
2018-11-28 00:44:13 +0300

    create output index.js file

---
+++ Auctionator/Auctionator/App/index.js
@@ -0,0 +1,20 @@
+﻿import Vue from 'vue'
+import AuctionItem from './components/AuctionItem.vue'
+import App from './App.vue'
+
+Vue.component('auctionItem', AuctionItem)
+
+new Vue({
+    el: '#app',
+    template: '<div><App/></div>',
+    components: {
+        'App': App
+    },
+    data: {
+        items: [
+            { message: 'Foo' },
+            { message: 'Bar' },
+            { message: App }
+        ]
+    }
+});
