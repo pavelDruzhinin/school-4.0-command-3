69692100f23c83a1a34755f4665bf7e188f2521b
igorjouk <igrokk70@gmail.com>
2018-12-10 00:04:52 +0300

    presented navbar element

---
+++ Auctionator/Auctionator/App/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div>
+      <a class="navbar-brand" href="#">
+        <img
+          src="https://crimea-news.com/img/20180718/344102b2345b120a47feea2de58fa131.jpg"
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt
+        >
+        Auctionator
+      </a>
+    </div>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        
+      </ul>
+    </div>
+    <div>Container for user</div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+</style>
