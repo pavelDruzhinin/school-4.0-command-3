035efd8304b4303ad0e17c143d43f9cc4c3a1740
Artyom <n-e-x@yandex.ru>
2018-12-10 12:43:19 +0300

    Добавил нужные миграции для товара

--- Auctionator/Auctionator/Models/Product.cs
+++ Auctionator/Auctionator/Models/Product.cs
@@ -10,14 +10,14 @@ namespace Auctionator.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Price { get; set; }
+        public double? Price { get; set; }
         public IList<ProductPhoto> Photos { get; set; }
         public string Description { get; set; }
         public string ShortDescription { get; set; }
         public ProductStatus Status { get; set; }
 
         public IList<SubscribedProduct> SubscribedProducts { get; set; }
-        public int AuctionId { get; set; }
+        public int? AuctionId { get; set; }
         public Auction Auction { get; set; }
         public string OwnerId { get; set; }
         public User Owner { get; set; }
