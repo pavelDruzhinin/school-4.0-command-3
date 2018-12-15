c3a9eb947df78f161dbbceba11934ae713becbe9
igorjouk <igrokk70@gmail.com>
2018-12-16 02:57:30 +0300

    get top 10 auctions

--- Auctionator/Auctionator/App/App.vue
+++ Auctionator/Auctionator/App/App.vue
@@ -10,22 +10,23 @@
 
 <script>
   import Auction from './components/Auction'
-  
+	import axios from 'axios'
+	
   export default { 
     
     data() {
       return {
-        auctions: [ { id: 1, title:'lot1', status: 'Started', startDateTime: '29.11.2018', endDateTime: '08.12.2018', startPrice: 34, lastBet: 34, productId:1, winnerId:'', image:'' },
-          { id: 2, title:'lot2', status: 'Started', startDateTime: '30.11.2018', endDateTime: '01.12.2018', startPrice: 434, lastBet: 434, productId:2, winnerId:'', image:'' },
-          { id: 3, title:'lot3', status: 'Started', startDateTime: '19.11.2018', endDateTime: '12.12.2018', startPrice: 1563, lastBet: 123, productId:3, winnerId:'', imgage:'' },
-          { id: 4, title:'lot4', status: 'Started', startDateTime: '19.11.2018', endDateTime: '12.12.2018', startPrice: 893, lastBet: 123, productId:3, winnerId:'', imgage:'' },
-          { id: 5, title:'lot5', status: 'Started', startDateTime: '19.11.2018', endDateTime: '12.12.2018', startPrice: 443, lastBet: 123, productId:3, winnerId:'', imgage:'' },
-          { id: 6, title:'lot6', status: 'Started', startDateTime: '19.11.2018', endDateTime: '12.12.2018', startPrice: 13, lastBet: 123, productId:3, winnerId:'', imgage:'' },
-          { id: 7, title:'lot7', status: 'Started', startDateTime: '19.11.2018', endDateTime: '12.12.2018', startPrice: 1233, lastBet: 123, productId:3, winnerId:'', imgage:'' }
-        ]
+        auctions: []
       }   
     },
-    components: {Auction}
+		components: {Auction},
+		beforeCreate() {
+			var that = this;
+			axios.get('auction/getformain/10')
+			.then(response => {
+				that.auctions = response.data.result;
+			})
+		}
 }
 </script>
 
